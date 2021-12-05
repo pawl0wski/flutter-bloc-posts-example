53f0edb8cb10945d70a55d788f378adf264bbccf
Jeboczek <kanakuba123359@gmail.com>
2021-12-05 12:05:46 +0100

    Create events and bloc for PostPage

--- lib/post_page/post_page_bloc.dart
+++ lib/post_page/post_page_bloc.dart
@@ -1,1 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+abstract class PostEvent {}
+
+class PostPageAddPost extends PostEvent {}
+
+class PostPageDeletePost extends PostEvent {}
+
+class PostPageBloc extends Bloc<PostEvent, List<Map<String, String>>> {
+  PostPageBloc() : super([]) {
+    on<PostPageAddPost>((event, emit) {
+      state.add({"author": "John", "content": "Hello, world!"});
+      emit(state.toList());
+    });
+
+    on<PostPageDeletePost>((event, emit) {
+      state.remove(state.last);
+      emit(state.toList());
+    });
+  }
+}
